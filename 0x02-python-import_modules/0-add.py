a8b59cbbefbc5ccb08b916511e961bec50963c54
ShawnZain <shawnzainpatel@gmail.com>
2023-11-02 10:10:46 -0700

    Adding a condition that ensures that the code in 0-add module will only execute when it is main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
-'''import function add() from module add_0
-only use add_0 once in your code
-'''
-from add_0 import add
+'''Your code should not be executed when imported - by using __import__'''
+if __name__ = "__main__":
 
-'''assign value of 1 to variable a and 2 to variable b
-in 2 separate lines
-'''
-a = 1
-b = 2
+    '''import function add() from module add_0
+    only use add_0 once in your code
+    '''
+    from add_0 import add
 
-'''print <a value> + <b value> = <add(a, b) value>'''
-print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    '''assign value of 1 to variable a and 2 to variable b
+    in 2 separate lines
+    '''
+    a = 1
+    b = 2
+
+    '''print <a value> + <b value> = <add(a, b) value>'''
+    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
