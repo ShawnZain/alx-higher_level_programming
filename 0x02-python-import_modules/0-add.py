b533230212e920d69fbd0a12bc2233d81d7b85d4
ShawnZain <shawnzainpatel@gmail.com>
2023-11-02 08:48:30 -0700

    Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+'''import function add() from module add_0
+only use add_0 once in your code
+'''
 from add_0 import add
 
+'''assign value of 1 to variable a and 2 to variable b
+in 2 separate lines
+'''
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a,b)))
+'''print <a value> + <b value> = <add(a, b) value>'''
+print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
