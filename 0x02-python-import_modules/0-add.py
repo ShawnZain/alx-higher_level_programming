eb9f9c31a846ef09e3c5ad073be128653318683c
ShawnZain <shawnzainpatel@gmail.com>
2023-11-02 10:16:04 -0700

    Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 
-'''Your code should not be executed when imported - by using __import__'''
-if __name__ = "__main__":
-
-    '''import function add() from module add_0
-    only use add_0 once in your code
-    '''
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
     from add_0 import add
 
-    '''assign value of 1 to variable a and 2 to variable b
-    in 2 separate lines
-    '''
     a = 1
     b = 2
-
-    '''print <a value> + <b value> = <add(a, b) value>'''
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
