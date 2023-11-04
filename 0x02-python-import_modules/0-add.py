0a74251d4cb31d12d704ae6518d384cf732964a2
ShawnZain <shawnzainpatel@gmail.com>
2023-11-04 10:38:00 -0700

    Modifying task 1 to run on the checker

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 
-'''Your code should not be executed when imported - by using __import__'''
 if __name__ == "__main__":
 
-    '''import function add() from module add_0
-    only use add_0 once in your code
-    '''
-    from add_0 import add
+    from add_0 import ad
 
-    '''assign value of 1 to variable a and 2 to variable b
-    in 2 separate lines
-    '''
     a = 1
     b = 2
+    result = add(a, b)
 
-    '''print <a value> + <b value> = <add(a, b) value>'''
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, result))
