a5830f7bb59fbadcccbff51391b6649a7b223e50
ShawnZain <shawnzainpatel@gmail.com>
2023-11-04 16:42:52 -0700

    Task 6: Importing a variable from a module

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from variable_load_5 import a
+
+
+def main():
+    """imports a variable from a module
+    and prints the variable
+    """
+    print(a)
+
+    
+if __name__ == "__main__":
+    main()
