cf19468935064be9e3a4f91b8ebb3c3ebe22ead0
ShawnZain <shawnzainpatel@gmail.com>
2023-11-06 14:39:47 -0800

    Task 8, add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Creates a new tuple and adds the
+    values of tuple_a[0] + tuple_b[0}
+    and tuple_a[1] + tuple_b[1]
+    and assign it to 0 & 1 index of the new tuple
+
+    Args:
+        tuple_a: add this
+        tuple_b: add this
+    """
+
+
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
