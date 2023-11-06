bad85742b744bc0520c9b06c930b15dab22136df
ShawnZain <shawnzainpatel@gmail.com>
2023-11-06 12:26:17 -0800

    PEP8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,12 @@
 
 
 def element_at(my_list, idx):
+    """function that retrieves the value at
+    a given index
+
+    my_list: list to retrieve value from
+    idx: index that we are looking for
+    """
     list_len = len(my_list)
 
     if (idx > list_len) or (idx < 0):
