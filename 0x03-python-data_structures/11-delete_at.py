49d9a196300f8d0470598c2a94e553d59286be75
ShawnZain <shawnzainpatel@gmail.com>
2023-11-06 15:24:40 -0800

    Task 12, the del statement

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    """deletes a value at given index
+
+    Args:
+        my_list: list to check for value
+        at given index to delete
+        idx: index to search for
+    """
+
+
+    if (idx < 0) or (idx > len(my_list)):
+        return my_list
+
+    del my_list[idx]
+
+    return my_list
