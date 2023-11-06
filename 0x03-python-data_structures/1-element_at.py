0b006279bd98efeec985dd2d640280ffad50893c
ShawnZain <shawnzainpatel@gmail.com>
2023-11-06 12:22:36 -0800

    Task 2, retrieve value at given index of a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    list_len = len(my_list)
+
+    if (idx > list_len) or (idx < 0):
+        return None
+    else:
+        return my_list[idx]
