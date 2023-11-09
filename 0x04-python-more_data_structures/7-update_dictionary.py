57cf787aeb6e52b942514d517fc7158ded82979c
ShawnZain <shawnzainpatel@gmail.com>
2023-11-09 15:49:19 -0800

    Task 8

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """changes the value of a key-value pair if it exists
+    adds the new key-value pair if it does not exist
+
+    Args:
+        a_dictionary: dict to alter
+        key: key to look for
+        value: value to change or add
+    """
+
+
+    a_dictionary[key] = value
