301e007db842d56e3c18b829e5cdecd424351102
ShawnZain <shawnzainpatel@gmail.com>
2023-11-09 15:30:12 -0800

    Task 6: How many keys are in the dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """how many keys are in the dictionary
+
+    Args:
+        a_dictionary: dictionary to check for keys
+    """
+
+
+    key_count = (len(a_dictionary.keys()))
+
+    return key_count
