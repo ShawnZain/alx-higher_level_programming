5faca459c35a5582d3dcaa1e3e06f913d714b81b
ShawnZain <shawnzainpatel@gmail.com>
2023-11-09 15:23:57 -0800

    Task 5

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 3
+#!/usr/bin/python3
 
 
 def only_diff_elements(set_1, set_2):
@@ -11,7 +11,6 @@ def only_diff_elements(set_1, set_2):
     """
 
 
-    unique_to_set1 = set_1 - set_2
-    unique_to_set2 = set_2 - set_1
+    unique_set = set(set_1 ^ set_2)
     
-    return unique_set1.union(unique_to_set2)
+    return unique_set
