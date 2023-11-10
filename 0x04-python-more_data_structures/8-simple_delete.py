6e09814a993401b02b14e14af8ecb2111abcbb7f
ShawnZain <shawnzainpatel@gmail.com>
2023-11-09 16:06:21 -0800

    Task 9 delete key-value pair

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    """deletes a key-value pair
+
+    Args:
+        a_dictionary: dict to manipulate
+        key: key to delete
+    """
+
+
+    return (del a_dictionary[key])
