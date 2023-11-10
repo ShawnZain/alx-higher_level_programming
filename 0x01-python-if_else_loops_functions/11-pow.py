ddc54a880ac5850ddc794848fc50161466b4bbff
ShawnZain <shawnzainpatel@gmail.com>
2023-11-10 12:27:40 -0800

    Power with negative numbers

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,4 +2,11 @@
 
 
 def pow(a, b):
-    return("{:d}".format(a ** b))
+
+    #check if a is negative
+    if a < 0:
+        return("{:d}".format(-1 * (abs(a) ** b)))
+    elif b < 0:
+        return("{:f}".format(1 / (a ** abs(b))))
+    else:
+        return("{:d}".format(a ** b))
