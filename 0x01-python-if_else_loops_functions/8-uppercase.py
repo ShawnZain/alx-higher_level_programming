3c9f56d581b0facddddd0dd1fc79907a8d3eb416
ShawnZain <shawnzainpatel@gmail.com>
2023-11-10 11:50:37 -0800

    Convert all lowercase to uppercase without importing a module. Use only the ord() function

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """checks to see in a character
+    in the given string is lowercase then converts it to uppercase
+
+    We cannot import any module
+
+    Args:
+        str: string to check for
+    """
+
+
+    #loop through the string to check for lowercase letters
+    for chars in str:
+        if 97 <= ord(chars) <= 122: # lowercase is from 97 to 122
+            chars = chr(ord(chars) - 32) #get the uppercase then cast to char
+
+        print(f"{chars}", end="")
+
+    print()
