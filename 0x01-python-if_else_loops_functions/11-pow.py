4bc1c95b0bba75e367a4337e99a8e7eedb5f6684
ShawnZain <shawnzainpatel@gmail.com>
2023-11-10 12:32:59 -0800

    SImplicity is the best

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,10 +3,4 @@
 
 def pow(a, b):
 
-    #check if a is negative
-    if a < 0:
-        return("{}".format(-1 * (abs(a) ** b)))
-    elif b < 0:
-        return("{}".format(1 / (a ** abs(b))))
-    else:
-        return("{}".format(a ** b))
+    return (a ** b)
