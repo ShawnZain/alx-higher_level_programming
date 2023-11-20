737f187e1c8d81853ad031d180a5913566e5661d
ShawnZain <shawnzainpatel@gmail.com>
2023-11-20 13:44:39 -0800

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """checks if input is an integer
+    it will print the int
+
+    Args:
+        value (int): value to check if it is int
+
+    Return:
+        True if it is int
+        False it is not
+    """
+
+    try:
+        print("{:d}".format(int(value)))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
