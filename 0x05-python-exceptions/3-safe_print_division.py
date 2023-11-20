2a10b0b6fd4c24cf9efde30fb125863f21b3ceb6
ShawnZain <shawnzainpatel@gmail.com>
2023-11-20 14:19:37 -0800

    Task 4

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """divides 2 numbers provided as args
+    Returns:
+        The value after division
+        Otherwise None
+    """
+    try:
+       result = a / b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Internal result: {}".format(result))
+    return (result)
