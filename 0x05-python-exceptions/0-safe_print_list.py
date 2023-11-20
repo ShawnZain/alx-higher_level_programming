e401f5f1b61ec0a7ac30e856c067065af0db7216
ShawnZain <shawnzainpatel@gmail.com>
2023-11-20 15:31:29 -0800

    Redoing task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,7 +17,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end = "")
             count += 1
-    except (IndexError, ValueError, TypeError):
+    except Exception:
         """IndexError occurs when the list does not have enough elements
         ValueError occurs when x is not an int
         """
