77caf056d7d85535c963a90ea94059c0ec93c383
ShawnZain <shawnzainpatel@gmail.com>
2023-11-20 13:45:24 -0800

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -13,7 +13,7 @@ def safe_print_integer(value):
     """
 
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
         return (False)
