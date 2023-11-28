732bfb0a5369f8520ce12ca2b4deaeffa0bbcc18
ShawnZain <shawnzainpatel@gmail.com>
2023-11-28 13:21:29 -0800

    Creating the Square class with private square attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define a square class"""
+
+
+class Square:
+    """Representing a square"""
+
+    def __init__(self, size):
+        """constructor method
+
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
