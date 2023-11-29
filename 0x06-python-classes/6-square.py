11d6efa03b72cd9f1cc4ef42db47bdf1123943b1
ShawnZain <shawnzainpatel@gmail.com>
2023-11-28 16:18:44 -0800

    Fixing type where size is refered to without self.__size

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,9 +40,9 @@ class Square:
             It must be an integer
             Size should always be greater than 0
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        elif size < 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
 
