90f132a2e4678022e77eb4beee96a00653e238a6
ShawnZain <shawnzainpatel@gmail.com>
2023-11-28 16:30:18 -0800

    Fixing the code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,15 +79,10 @@ class Square:
 
         if self.__size == 0:
             print("")
+            return
 
-        for new_line in range(0, self.__position[1]):
-            print("")
-
-        for length in range(0, self.__size):
-            for space in range(0, self.__position[0]):
-                print(" ", end = "")
-
-            for width in range(0, self.__size):
-                print("#", end = "")
-
+         [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
